65ef069094fac95715900a73fb3e690b20341edf
javipani12 <javier.fernandez.paniagua.22@gmail.com>
2023-02-10 14:40:06 +0100

    clase 10/2/23

--- test/TestFicha.java
+++ test/TestFicha.java
@@ -1,3 +1,7 @@
+import modelos.Departamento;
+import modelos.Empleado;
+import modelos.Ficha;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
 /**
@@ -9,6 +13,41 @@ public class TestFicha {
     
     @Test
     public void testCrearFicha(){
+        Departamento dept = new Departamento();
         
+        if (dept == null) {
+            fail("Fallo testCrearFicha (Error instanciando Departamento");
+        }
+        
+        if (dept.getIdDepartamento() != -1 || dept.getNombre() != null) {
+            fail("Fallo testCrearFicha (Fallo Constructor Departamento (Valor por defecto)");
+
+        }
+        
+        Empleado emp = new Empleado();
+        
+        // Comprobaciones para saber si está bien instanciado
+        if( emp == null ){
+            // resultado = false;
+            fail("Fallo testCrearFicha (Error instanciando Empleado)");
+        }
+        
+        if( emp.getIdEmpleado() != -1 || emp.getNombre() != null || 
+            emp.getApellido() != null || emp.getSalario() != 0F ||
+            emp.getEmail() != null || emp.getDpt() != null)
+        {
+            fail("Fallo Constructor (Valores por defecto)");
+        }
+        
+        
+        Ficha ficha = new Ficha();
+        
+        if (ficha == null) {
+            fail("Fallo en testCrearFicha (Error instanciando Ficha)");
+        }
+        
+        if ( ficha.getIdFicha() != -1 ) {
+            
+        }
     }
 }
